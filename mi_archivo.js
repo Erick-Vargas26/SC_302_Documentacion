dfa1cc02e5c977ba4814cfdbc4a4580dc0907454
Erick-Vargas26 <145409443+Erick-Vargas26@users.noreply.github.com>
2023-09-18 20:11:40 -0600

    mi_archivo.js

--- mi_archivo.js
+++ mi_archivo.js
@@ -2,5 +2,5 @@ Alert(“Hola mundo_editado”)
 Console.log (“cambios en el archivo”)
 //"Comentario del punto B"
 //"https://gist.github.com/Erick-Vargas26/8568f2a85fdd3afa67df43a38278439b"
-//
+//https://github.com/users/Erick-Vargas26/projects/7/views/1
 Commit changes
